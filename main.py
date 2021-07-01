a4d26730ed26c0244692740ac11e82d5ede480db
noob-coder111 <77723878+noob-coder111@users.noreply.github.com>
2021-07-01 17:13:37 +0530

    added multiple tables and features

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 import boto3
-from pydantic.main import BaseModel
-import json
-from pydantic import BaseModel
-from typing import List
+# from pydantic.main import BaseModel
+import jsons
+# from pydantic import BaseModel
+# from typing import List
 from fastapi import FastAPI
 from boto3.dynamodb.conditions import Key,Attr
 # from fastapi.routing import APIRoute, APIRouter
@@ -43,36 +43,76 @@ def Records(Query,Name):
 
 
 
-TABLE_NAME2 = "resultTable2"
+
+
+TABLE_NAME2 = "Doctor"
 # Creating the DynamoDB Table Resource
 dynamodb = boto3.resource('dynamodb', region_name="ap-south-1")
-table = dynamodb.Table(TABLE_NAME)
+table = dynamodb.Table(TABLE_NAME2)
 
-@app.get('/Personal details/{Doctor}/{Patient}')
+@app.get('/Personal details//{Doctor}')
 def Record(Name):
   
-    respons = table.scan(
+    respons2 = table.scan(
                         TableName=TABLE_NAME2,
                         #KeyConditionExpression=Key("").eq("1"),
                         FilterExpression=Attr("Name").eq(Name)
                         )
-    item = respons['Items']
+    item = respons2['Items']
     print(item)
     return item
 
 
-@app.put('/Personal details/{Doctor}/{Patient}')
-def EnterRecord(ID:str, Name:str, Age:str, Patient:str, Doctor:str):
-    table = dynamodb.Table('resultTable2')
+@app.put('/Personal details///{Doctor}')
+def EnterRecord(ID:str, Name:str, Phone:str, Email:str, Password:str, Gender:str, Age:str, Experience:str, Designation:str):
+    table = dynamodb.Table('Doctor')
     response = table.put_item(
        Item={
             'ID':ID,
             'Name':Name,
+            'Phone':Phone,
+            'Email':Email,
+            'Password':Password,
+            'Gender':Gender,
             'Age':Age,
-            'Patient':Patient,
-            'Doctor':Doctor
+            'Experience':Experience,
+            'Designation':Designation
             }
     )
     return "Data added successfully"
 
+TABLE_NAME3 = "Patient"
+# Creating the DynamoDB Table Resource
+dynamodb = boto3.resource('dynamodb', region_name="ap-south-1")
+table = dynamodb.Table(TABLE_NAME3)
 
+@app.get('/Personal details////{Patient}')
+def Record2(Name):
+  
+    respons2 = table.scan(
+                        TableName=TABLE_NAME3,
+                        #KeyConditionExpression=Key("").eq("1"),
+                        FilterExpression=Attr("Name").eq(Name)
+                        )
+    item2 = respons2['Items']
+    print(item2)
+    return item2
+
+
+@app.put('/Personal details/////{Patient}')
+def EnterRecord2(ID:str, Name:str, Phone:str, Email:str, Password:str, Gender:str, Age:str, Complaints:str, Previous_Ailments:str):
+    table = dynamodb.Table('Patient')
+    response = table.put_item(
+       Item={
+            'ID':ID,
+            'Name':Name,
+            'Phone':Phone,
+            'Email':Email,
+            'Password':Password,
+            'Gender':Gender,
+            'Age':Age,
+            'Complaints':Complaints,
+            'Previous_Ailments':Previous_Ailments
+            }
+    )
+    return "Data added successfully"
